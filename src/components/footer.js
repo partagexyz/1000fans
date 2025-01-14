41cce1bd85252b5ea414185c38cada61fe420548
jcarbonnell <julien.carbonnell@gmail.com>
2025-01-14 08:05:02 +0100

    still working on fixing the server rendering. now fonts is cards turn to black or white based on the light or dark theme

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,43 +3,42 @@ import React, { useState, useEffect } from 'react';
 import styles from '@/styles/app.module.css';
 
 const Footer = () => {
-  const [currentTheme, setCurrentTheme] = useState('light');
+  const [currentTheme, setCurrentTheme] = useState('light'); // Default to light
 
   useEffect(() => {
+    // Get the saved theme or system preference on mount
     const savedTheme = localStorage.getItem('theme');
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const initialTheme = savedTheme || (prefersDark ? 'dark' : 'light');
+
+    // Apply the theme and set state
     document.documentElement.setAttribute('data-theme', initialTheme);
     setCurrentTheme(initialTheme);
-  }, [currentTheme]);
+  }, []);
 
   const toggleTheme = () => {
+    // Toggle between 'dark' and 'light' themes
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
     setCurrentTheme(newTheme);
-  
-    // Disable transitions for immediate color change
-    document.querySelectorAll('.card p').forEach(p => {
-      p.style.transition = 'none';
-      p.style.color = newTheme === 'light' ? 'black' : 'white';
-      setTimeout(() => {
-        p.style.transition = ''; // Reset transitions
-      }, 0);
-    });
   };
 
   return (
     <footer className={styles.footer}>
       <p>
-        Made by artists, for artists. {' '}
-        © 
+        Made by artists, for artists.{' '}
+        ©{' '}
         <a href="https://1000Fans.xyz" target="_blank" rel="noopener noreferrer">
           1000Fans.xyz
-        </a>{', '}
-        {new Date().getFullYear()}.
+        </a>
+        , {new Date().getFullYear()}.
       </p>
-      <button onClick={toggleTheme} aria-label="Toggle dark mode" className={`${styles['theme-toggle']} ${styles['theme-toggle-footer']}`}>
+      <button
+        onClick={toggleTheme}
+        aria-label="Toggle dark mode"
+        className={`${styles['theme-toggle']} ${styles['theme-toggle-footer']}`}
+      >
         <i className={`bi ${currentTheme === 'dark' ? 'bi-sun-fill' : 'bi-moon-fill'}`}></i>
       </button>
     </footer>
