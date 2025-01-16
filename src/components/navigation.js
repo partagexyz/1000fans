20844daaee846249c70e585e0cf7e64c982275a4
jcarbonnell <julien.carbonnell@gmail.com>
2025-01-16 20:07:08 +0100

    handling no background no logo situation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState, useContext } from 'react';
 import { NearContext } from '@/wallets/near';
-import Icon from '/public/favicon.ico';
 import styles from '@/styles/app.module.css';
 
 export const Navigation = () => {
@@ -28,13 +27,15 @@ export const Navigation = () => {
       <div className={styles['navbar-content']}>
         <Link href="/" passHref legacyBehavior>
           <div className={styles['navbar-brand']}>
+            {/* Use a string path instead of importing the icon directly */}
             <Image 
               priority 
-              src={Icon} 
+              src="/favicon.ico" 
               alt="Theosis Icon" 
               width={51} 
               height={51} 
               className={styles['navbar-icon']} 
+              onError={(e) => { e.target.style.display = 'none'; }} // Hide if image fails to load
             />
             {/*<span className={`${styles['navbar-brand']} mb-0 h1 ms-2`}>1000 FANS</span>*/}
           </div>
